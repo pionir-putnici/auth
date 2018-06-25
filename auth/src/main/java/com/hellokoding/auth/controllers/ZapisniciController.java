9348e4f57343c9d5ab4bd4f8f091334f88d25632
Ins1ck <norbertmahalik@gmail.com>
2018-06-25 14:14:54 +0200

    25.06.2018. Dodavanje Zapisnika
    Zapisnici kontroler
    Zapisnici repository

---
+++ auth/src/main/java/com/hellokoding/auth/controllers/ZapisniciController.java
@@ -0,0 +1,31 @@
+package com.hellokoding.auth.controllers;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+//import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsPdfView;
+import com.hellokoding.auth.repository.ZapisniciRepository;
+
+
+@Controller
+public class ZapisniciController {
+
+	
+}
